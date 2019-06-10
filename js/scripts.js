b872a9c2622b0c3c472601fac1e86ca523035046
Yomi Rich <ymueni@gmail.com>
2019-06-10 12:51:03 +0300

    Add indentation and correct syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-
-var whatWeDo = ['design','development','product'];
-   whatWeDo.forEach(function(what){
-       $("#"+what).click(function (){
-           $("#"+what).hide();
-           $("#"+what+".ps").show();
-       });
-       $("#"+what+".ps").click(function (){
-           $("#"+what+".ps").hide();
-           $("#"+what).show();
-       });
+$(document).ready(function() {
+  $(".d2").hide();
+ $(".design").click(function() {
+   $(".design").toggle();
+   $(".d2").click(function() {
+     $(".design").toggle();
+     });
    });
+});
