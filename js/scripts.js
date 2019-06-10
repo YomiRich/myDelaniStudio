c1dfdd1f0f68fca84495fff6bf2e33ed5280365b
Yomi Rich <ymueni@gmail.com>
2019-06-10 19:59:00 +0300

    Add all changes

--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,58 @@ $("#development-p").on('click', function(){
     $("#product-p").hide();
     $("#product-img").show();
     });
-});
+    $(".t1").mouseenter(function(){
+        $(".text1").show();
+    })
+    $(".t1").mouseleave(function () {
+        $(".text1").hide();
+    })
+    $(".t1").mouseenter(function(){
+        $(".text1").show();
+    })
+    $(".t1").mouseleave(function () {
+        $(".text1").hide();
+    })
+    $(".t2").mouseenter(function(){
+        $(".text2").show();
+    })
+    $(".t2").mouseleave(function () {
+        $(".text2").hide();
+    })
+    $(".t3").mouseenter(function(){
+        $(".text3").show();
+    })
+    $(".t3").mouseleave(function () {
+        $(".text3").hide();
+    })
+    $(".t4").mouseenter(function(){
+        $(".text4").show();
+    })
+    $(".t4").mouseleave(function () {
+        $(".text4").hide();
+    })
+    $(".t5").mouseenter(function(){
+       $(".text5").show();
+   })
+   $(".t5").mouseleave(function () {
+       $(".text5").hide();
+})
+$(".t6").mouseenter(function(){
+   $(".text6").show();
+})
+$(".t6").mouseleave(function () {
+   $(".text6").hide();
+})
+$(".t7").mouseenter(function(){
+   $(".text7").show();
+})
+$(".t7").mouseleave(function () {
+   $(".text7").hide();
+})
+$(".t8").mouseenter(function(){
+   $(".text8").show();
+})
+$(".t8").mouseleave(function () {
+   $(".text8").hide();
+})
+})
