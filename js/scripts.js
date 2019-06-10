1c9d66de8d6e317ce8d00b7a6803942beaf15ba6
Yomi Rich <ymueni@gmail.com>
2019-06-10 15:07:45 +0300

    Add toggle to development id

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-  $(".d2").hide();
- $(".design").click(function() {
-   $(".design").toggle();
-   $(".d2").click(function() {
-     $(".design").toggle();
-     });
-   });
+$(document).ready(function(){
+  $("#design-img").on('click', function(){
+    $("#design-img").hide();
+  $("#design-p").show();
+});
+  $("#design-p").on('click', function(){
+    $("#design-p").hide();
+    $("#design-img").show();
+  });
+
+$("#development-img").on('click', function(){
+  $("#development-img").hide();
+$("#devolopment-p").show();
+});
+$("#development-p").on('click', function(){
+  $("#development-p").hide();
+  $("#development-img").show();
+  });
 });
